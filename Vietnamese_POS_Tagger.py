168c8f5f4426c19d07dbce7090dbbce23c2fa9d4
Reebal Abelson <rabelson@ucsd.edu>
2017-06-05 00:18:54 -0700

    Added more features

--- Vietnamese_POS_Tagger.py
+++ Vietnamese_POS_Tagger.py
@@ -27,61 +27,141 @@ tagsetDict = {"Np" : 0,
               "Y" : 15,
               "Z" : 16,
               "X" : 17 }
+inverseTagsetDict = {tagsetDict[k]:k for k in tagsetDict}
 
 wordBank = defaultdict()
-f_tagged = io.open("corpus/VNTQcorpus-small.tagged.txt", encoding='utf-8').read()
-f = io.open("corpus/VNTQcorpus-small.txt", encoding='utf-8').read()
+bigramBank = defaultdict()
+bigramDict = defaultdict(int)
+
+bigramFreq = {x:[0]*18 for x in tagsetDict}
+mostCommonBigrams = defaultdict()
+
+prevParts = None
+
+f_tagged = io.open("corpus/VNTQcorpus-small.tagged.txt", encoding='utf-8').readlines()
+# f = io.open("corpus/VNTQcorpus-small.txt", encoding='utf-8').readlines()
 
 # Separate train and test set
 # train = f_tagged[:int(len(f)/2)]
 # test = f_tagged[int(len(f)/2):]
-train = f_tagged[:50000]
-test = f_tagged[50000:100000]
+train = f_tagged[:30000]
+test = f_tagged[30000:60000]
 
 # Get training data
-for w in train.split():
-    parts = w.split("/")
-    if len(parts) == 1 or parts[1] not in tagsetDict:
-        continue
-    word = parts[0]
-    pos = parts[1]
-    if word not in wordBank:
-        wordBank[word] = [pos]
-    else:
-        wordBank[word] += [pos]
+for line in train:
+    l_split = line.split()
+    for i,w in enumerate(l_split):
+        parts = w.split("/")
+        if i >= 1:
+            prevParts = l_split[i-1].split("/")
+        if len(parts) == 1 or \
+            parts[1] not in tagsetDict:
+            continue
+
+        # if prev parts is a punctuation get the one before that
+        if i >= 2 and prevParts[1] in string.punctuation:
+            prevParts = l_split[i-2].split("/")
+
+        word = parts[0]
+        pos = parts[1]
+
+        if i >= 1:
+            prevWord = prevParts[0]
+            prevPos = prevParts[1]
+            bigramBank[ word ] = (pos, prevWord, prevPos)
+            bigramDict[ (pos, prevPos) ] += 1
+            #calculate pos after pos
+            #bigramFreq[prevPos][tagsetDict[pos]] += 1
+
+        # Makes word map to list of pos tags
+        if word not in wordBank:
+            wordBank[word] = [pos]
+        else:
+            wordBank[word] += [pos]
+
+for k in bigramFreq:
+    maxFreq = 0
+    maxPos = "Np"
+    for i,x in enumerate(bigramFreq[k]):
+        if x > maxFreq:
+            maxFreq = x
+            maxPos = inverseTagsetDict[i]
+    mostCommonBigrams[k] = maxPos
+
 
 print ("finished getting training data")
 
+
+#####################################################################
 # Features
-def feature(word):
+def feature(word,wordIdx, lineSize, line):
     feat = [1]
+
+    # print("word is : " + str(word))
+    # print("wordIdx is : " + str(wordIdx))
+    # print("linesize is : "  + str(lineSize))
+
+    # how far word is into sentence
+    sentPercent = float(wordIdx)/float(lineSize)
+    feat.append(sentPercent)
+
+    # 0.8516463095298431
+
+    if word[0].isupper() and wordIdx != 0:
+        feat.append(1)
+    else:
+        feat.append(0)
 
     # label pos tags for each word
     posIdx_array = ([0] * len(tagsetDict))
+    posSet = []
     if word in wordBank:
         posSet = wordBank[word]
     else:
-        posSet = list(tagsetDict.keys())[random.randint(0, 5)] # Naive: always choose N
-        posIdx_array[tagsetDict[posSet]] = 1
-        return feat + posIdx_array
+        # see word we don't know
+        if wordIdx == 0:
+            posSet = list(tagsetDict.keys())[0] # Naive: always choose Np for word we havent seen
+            posIdx_array[tagsetDict[posSet]] = 1
+            return feat + posIdx_array + [0]
+        else:
+            prevWord = line[wordIdx-1]  #TODO this is questionable
+            if prevWord in wordBank:
+                prevPos = wordBank[prevWord]
+                maxPos = mostCommonBigrams[prevPos]
+                # posIdx_array[tagsetDict[maxPos]] = 1
+                feat += posIdx_array + [tagsetDict[maxPos]]
+                return feat
+            else:
+                posSet = list(tagsetDict.keys())[0]  # Naive: always choose Np for word we havent seen
+                # posIdx_array[tagsetDict[posSet]] = 1
+                feat += posIdx_array + [0]
+                return feat
 
     for pos in posSet:
         posIdx = tagsetDict[pos]
         posIdx_array[posIdx] += 1.0/len(wordBank[word])
-    feat += (posIdx_array)
+    feat += (posIdx_array) + [0]
+
+    # Don't put features here!
 
     return feat
+#####################################################################
+
 
 # Create y (list of pos tags) and x (feature) data
 y = []
 X_train = []
-for w in train.split():
-    parts = w.split("/")
-    word = parts[0]
-    if len(parts) == 1 or parts[1] not in tagsetDict:
-        continue
-    y.append(wordBank[word][0]) # [0] for first word tagged pos
-    X_train.append(feature(word))
+for line in train:
+    l_split = line.split()
+    for i,w in enumerate(l_split):
+        parts = w.split("/")
+        word = parts[0]
+        len_line = len(l_split)
+
+        if len(parts) == 1 or parts[1] not in tagsetDict:
+            continue
+        y.append(wordBank[word][0]) # [0] for first word tagged pos
+        X_train.append(feature(word,i,len_line, l_split))
 
 print ("finished creating y and xtrain")
 
@@ -96,21 +176,25 @@ print ("finished fitting")
 X_test = []
 correct_results = []
 # Get testing data
-for w in test.split():
-    parts = w.split("/")
-    if len(parts) == 1 or parts[1] not in tagsetDict:
-        continue
-    word = parts[0]
-    pos = parts[1]
-    if word in string.punctuation or word == ":.":
-        continue
-    X_test.append(feature(word))
-    correct_results.append(pos)
+for line in test:
+    l_split = line.split()
+    for i,w in enumerate(l_split):
+        len_line = len(l_split)
+        parts = w.split("/")
+        if len(parts) == 1 or parts[1] not in tagsetDict:
+            continue
+        word = parts[0]
+        pos = parts[1]
+        if word in string.punctuation or word == ":.":
+            continue
+        X_test.append(feature(word,i,len_line,l_split))
+        correct_results.append(pos)
 
 print("finished getting testing data")
 
 predicted_results = train_fit.predict(X_test)
-print(predicted_results)
+print(predicted_results[:10])
+print(correct_results[:10])
 
 numCorrect = 0
 for cor,pred in zip(correct_results, predicted_results):
